9400a1e96b6661c52074ced618c41134bddf3d22
Juan Yu <sigrace0417@gmail.com>
2026-01-16 23:52:16 +0200

    modify git.js path

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import { filterRepos } from "./processors/filter.js";
 import { rankRepos } from "./processors/rank.js";
 import { summarizeRepos } from "./llm/summarizer.js";
 import { generateMarkdown, saveDailyMarkdown } from "./generators/markdown.js";
-import { commitAndPushDaily } from "./utils/git.js";
+import { commitAndPushDaily } from "./services/git.js";
 import { mailer } from "./notifications/mailer.js";
 
 const date = new Date().toISOString().slice(0, 10);
